b7883bf363fe4af413e9854ff70e73243de59ad7
Estlib <75094683+Estlib@users.noreply.github.com>
2026-09-23 08:43:04 +0300

    Testide loomisel tekkis kahe erineva projekti struktuuri liitmisel probleem, kus alusraamistik täielikult erineb, see on katse ehitada mingisugune vahelüli mis lubaks sillastada kaks erinevat struktuuri kahel erineval raamistikul

---
+++ TARge25Shop/TARge25Shop.XUnitTesting/AdaptedDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+
+namespace TARge25Shop.XUnitTesting
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+    }
+    public class AdaptedDbContext : IdentityDbContext<ApplicationUser>
+    {
+
+    }
+}
+//https://github.com/AnastasiiaRadasheva/kooli/blob/testimine/Kool/Models/IdentityModels.cs
